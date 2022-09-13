31b9f1b45fb4d653454ab27cd80f5fc999e612e3
admin <murtuza.xworkz@gmail.com>
2022-09-13 23:36:26 +0530

    13 sep

--- IdentityRunner.java
+++ IdentityRunner.java
@@ -1,36 +1,340 @@
 class IdentityRunner{
 	
-	public static void main(String[] info)
-	{	
-		System.out.println("Executing PrintName Method");
-		String a1="Furqan";   				//Explicit Reference
-		Identity.printName(a1);
-		String a2="Ayaz";					// Expilicit Reference
-		Identity.printName("Salman");  	                                   		
-		Identity.printName("Rahul");
-		Identity.printName("Maruti");		 // Implicit Reference
-		Identity.printName("Ganesh");
-		Identity.printName("Pavan");
-		Identity.printName("Vinod");
-		Identity.printName("Manju");
-		Identity.printName("Ravi");
+	
+	public static void main(String[]names)
+	{
 		
-		System.out.println("Executing nameAndEmail Method");
-		String name= "Haji";          		  //Declared and initialized
-		String email="haji123@gmail.com";
-		Identity.nameAndEmail(name,email);   
+		System.out.println("Details of persons");
 		
-				name="Ganesh";              // re-initializing
-				email="Ganesh123@gmail.com";
-				Identity.nameAndEmail(name,email);		// Explicit Reference
-				
-				name ="Maruti";
-				email="Maruti123@gmail.com";
-				Identity.nameAndEmail(name,email);
-				Identity.nameAndEmail("Mahesh","mahesh123@gmail.com");    // Implicit Reference
-				Identity.nameAndEmail("Pavan","pavan123@gmail.com");
-				Identity.nameAndEmail("Mahesh","mahesh123@gmail.com");
-				
-				
+		
+		
+		
+		String X ="Yasmin";
+		Identity.printName(X);
+		
+	     String x1 ="Saniya";
+		 Identity.printName(x1);
+		 
+		 String x2 ="Neha";
+		 Identity.printName(x2);
+		 
+		 
+		 String x3 ="Tabbu";
+		 Identity.printName(x3);
+		 
+		 String x4 ="Pari";
+		 Identity.printName(x4);
+		 
+		 String x5 ="Jyoti";
+		 Identity.printName(x5);
+		 
+		 
+		 String x6 ="Abdul";
+		 Identity.printName(x6);
+		 
+		 Identity.printName("Arfa");
+		 
+		 Identity.printName("Namrata");
+		 
+		 Identity.printName("Alisha");
+		 Identity.printName("Sindhu");
+		 Identity.printName("Haji");
+		 Identity.printName("Moeen");
+		 
+		 System.out.println("LIST OF NAMES WITH EMAIL");
+		 
+		 
+		 String name1= "Yasmin";
+		 String email1="yasminxworkz@gmail.com";
+		 
+		 Identity.printEmail(name1,email1);
+		 
+		 String name2="Saniya";
+		 String email2="Saniya22@gmail.com";
+		 Identity.printEmail(name2,email2);
+		 
+		 
+		 String name3="Neha";
+		 String email3="Neha26@gmail.com";
+		 Identity.printEmail(name3,email3);
+		 
+		 
+		 String name4="Tabbu";
+		 String email4="Tabbu28@gmail.com";
+		 Identity.printEmail(name4,email4);
+		 
+		 
+		 String name5="Pari";
+		 String email5="Pari52@gmail.com";
+		 Identity.printEmail(name5,email5);
+		 
+		 
+		 String name6="Jyoti";
+		 String email6="Jyotixworkz@gmail.com";
+		 Identity.printEmail(name6,email6);
+		 
+		 
+		 String name7="Arfa";
+		 String email7="Arfa25@gmail.com";
+		 Identity.printEmail(name7,email7);
+		 
+		 
+		 String name8="Namrata";
+		 String email8="Namrata20@gmail.com";
+		 Identity.printEmail(name8,email8);
+		 
+		 
+		 String name9="Alisha";
+		 String email9="Alisha27@gmail.com";
+		 Identity.printEmail(name9,email9);
+		 
+		 
+		 String name11="Sindhu";
+		 String email11="Sindhu23@gmail.com";
+		 Identity.printEmail(name11,email11);
+		 
+		 
+		 String name12="Swati";
+		 String email12="Swati21@gmail.com";
+		 Identity.printEmail(name12,email12);
+		 
+		 
+		 String name13="Abdul";
+		 String email13="Abdul24@gmail.com";
+		 Identity.printEmail(name13,email13);
+		 
+		 System.out.println("LIST OF NAMES WITH PHONE NUMBERS");
+		 
+		 String a1="Vaani";
+		 long phoneNumber1=8674293012l;
+		 Identity.printPhoneNumber(a1,phoneNumber1);
+		 
+		 String a2="Preeti";
+		 long phoneNumber2=9430806214l;
+		 Identity.printPhoneNumber(name2,phoneNumber2);
+		 
+		 String a3="Keerti";
+		 long phoneNumber3=8660849342l;
+		 Identity.printPhoneNumber(name3,phoneNumber3);
+		 
+		 String a4="Kavya";
+		 long phoneNumber4=9803503654l;
+		 Identity.printPhoneNumber(name4,phoneNumber4);
+		 
+		 String a5="Akshara";
+		 long phoneNumber5=9977223508l;
+		 Identity.printPhoneNumber(name5,phoneNumber5);
+		 
+		 
+		 String a6="Hussain";
+		 long phoneNumber6=8456329612l;
+		 Identity.printPhoneNumber(name6,phoneNumber6);
+		 
+		 String a7="Maimoon";
+		 long phoneNumber7=9886571798l;
+		 Identity.printPhoneNumber(a7,phoneNumber7);
+		 
+		 
+		 String a8="Haji";
+		 long phoneNumber8=9836472142l;
+		 Identity.printPhoneNumber(a8,phoneNumber8);
+		 
+		 String a9=" Farahath";
+		 long phoneNumber9=9254637091l;
+		 Identity.printPhoneNumber(a9,phoneNumber9);
+		 
+		 
+		 String a11="Nadeem";
+		 long phoneNumber11=7324516892l;
+		 Identity.printPhoneNumber(a11,phoneNumber11);
+		 
+		 String a12="Heena";
+		 long phoneNumber12=8096243120l;
+		 Identity.printPhoneNumber(a12,phoneNumber12);
+		 
+		 String a13="Almas";
+		 long phoneNumber13=9932185420l;
+		 Identity.printPhoneNumber(a13,phoneNumber13);
+		 
+		 
+		 System.out.println("LIST OF NAMES WITH AGE");
+		 
+		 
+		 
+		 
+		 a1="Vaani";
+		 int age1=26;
+		 Identity.printAge(a1,age1);
+		 
+		  a2="Preeti";
+		 int age2=24;
+		 Identity.printAge(a2,age2);
+		 
+		 
+		 
+		  a3="Keerti";
+		 int age3=26;
+		 Identity.printAge(a3,age3); 
+		 
+		  a4="Kavya";
+		 int age4=21;
+		 Identity.printAge(a4,age4);
+		 
+		  a5="Akshara";
+		 int age5=24;
+		 Identity.printAge(a5,age5);
+		 
+		 
+		 
+		  a6="Hussain";
+		 int age6=25;
+		 Identity.printAge(a6,age6);
+		 
+		 
+		  a7="Maimoon";
+		 int age7=22;
+		 Identity.printAge(a7,age7);
+		 
+		 
+		 
+		  a8="Haji";
+		 int age8=21;
+		 Identity.printAge(a8,age8);
+		 
+		 
+		  a9=" Farahath";
+		 int age9=20;
+		 Identity.printAge(a9,age9);
+		 
+		 
+		  a11="Nadeem";
+		 int age11=28;
+		 Identity.printAge(a11,age11);
+		 
+		  a12="Heena";
+		 int age12=27;
+		 Identity.printAge(a12,age12);
+		 
+		 
+		  a13="Almas";
+		 int age13=21;
+		 Identity.printAge(a13,age13);
+		 
+		 
+		  System.out.println("LIST OF NAMES WITH ADDRESS");
+		  
+		   a1="Vaani";
+		   String address="J C nagar, bangalore";
+		   Identity.printAddress(a1,address);
+		   
+		   Identity.printAddress("Preeti","Basveshwaranagar Bangalore");
+		   
+		   Identity.printAddress("jaya","Betageri Gadag");
+		   Identity.printAddress("Prema","Rajivgandhinagar Gadag");
+		   Identity.printAddress("Saraswati","kurattipet Gadag");
+		   Identity.printAddress("Deepa","whitefield Bangalore");
+		   Identity.printAddress("Arman","hudko Gadag");
+		   Identity.printAddress(a7,"kaveri nagar Bangalore");
+		   Identity.printAddress(a8,"Navarang Bangalore");
+		   Identity.printAddress(a9,"Rajajinagar Bangalore");
+		   Identity.printAddress(a11,"Yelhanka Bangalore");
+		   Identity.printAddress(a12,"J P Nagar Bangalore");
+		   
+		   System.out.println("LIST OF NAMES WITH EMAIL,ADDRESS AND AGE");
+		   
+		   Identity.printDetails(a1,email1,"J C nagar, bangalore",age1);
+		   Identity.printDetails(a2,email2,"Basveshwaranagar Bangalore",age2);
+		   Identity.printDetails(a3,email3,"Betageri Gadag",age3);
+		   Identity.printDetails(a4,email4,"Rajivgandhinagar Gadag",age4);
+		   Identity.printDetails(a5,email5,"kurattipet Gadag",age5);
+		   Identity.printDetails(a6,email6,"whitefield Bangalore",age6);
+		   Identity.printDetails(a7,email7,"hudko Gadag",age7);
+		   Identity.printDetails(a8,email8,"kaveri nagar Bangalore",age8);
+		   Identity.printDetails(a9,email9,"Navarang Bangalore",age9);
+		   Identity.printDetails(a11,email11,"Rajajinagar Bangalore",age11);
+		   Identity.printDetails(a12,email12,"Yelhanka Bangalore",age12);
+		   Identity.printDetails(a13,email13,"J P Nagar Bangalore",age13);
+		   
+		   
+		   System.out.println("LIST OF NAMES WITH GENDER");
+		   Identity.printGender(a1,'F');
+		   Identity.printGender(a2,'F');
+		   Identity.printGender(a3,'F');
+		   Identity.printGender(a4,'F');
+		   Identity.printGender(a5,'F');
+		   Identity.printGender(a6,'M');
+		   Identity.printGender(a7,'F');
+		   Identity.printGender(a8,'M');
+		   Identity.printGender(a9,'F');
+		   Identity.printGender(a11,'M');
+		   Identity.printGender(a12,'F');
+		   Identity.printGender(a13,'F');
+		   
+		   System.out.println("LIST OF NAMES WITH WORKING STATUS");
+		   
+		   Identity.printWork(a1,true);
+		   Identity.printWork(a2,true);
+		   Identity.printWork(a3,false);
+		   Identity.printWork(a4,true);
+		   Identity.printWork(a5,true);
+		   Identity.printWork(a6,false);
+		   Identity.printWork(a7,true);
+		   Identity.printWork(a8,false);
+		   Identity.printWork(a9,true);
+		   Identity.printWork(a11,false);
+		   Identity.printWork(a12,true);
+		   Identity.printWork(a13,false);
+		   
+		   
+		   System.out.println("LIST OF NAMES WITH Dress size");
+		   
+		   Identity.printSize(a1,"XL");
+		   Identity.printSize(a2,"L");
+		   Identity.printSize(a3,"L");
+		   Identity.printSize(a4,"M");
+		   Identity.printSize(a5,"L");
+		   Identity.printSize(a6,"XL");
+		   Identity.printSize(a7,"L");
+		   Identity.printSize(a8,"M");
+		   Identity.printSize(a9,"M");
+		   Identity.printSize(a11,"L");
+		   Identity.printSize(a12,"XL");
+		   Identity.printSize(a13,"M");
+		   
+		   System.out.println("Printing Name and PantSize");
+		   Identity.printNameAndPantSize("haji",30);
+		   Identity.printNameAndPantSize("Ganesh",32);
+		   Identity.printNameAndPantSize("Raju",34);
+		   Identity.printNameAndPantSize("Maruti",30);
+		   Identity.printNameAndPantSize("Pavan",34);
+		   Identity.printNameAndPantSize("Raj",36);
+		   Identity.printNameAndPantSize("Basu",32);
+		   Identity.printNameAndPantSize("Sunil",30);
+		   Identity.printNameAndPantSize("Ayaz",34);
+		   Identity.printNameAndPantSize("Furqan",32);
+		   Identity.printNameAndPantSize("Nusrat",36);
+		   Identity.printNameAndPantSize("Ravi",34);
+
+
+
+
+		   System.out.println("Printing height and weight");
+		   Identity.printNameAndWeightAndHeight("haji",55.5d,5.9d);
+		   Identity.printNameAndWeightAndHeight("Ganesh",62.3d,5.5d);
+		   Identity.printNameAndWeightAndHeight("Raju",60.25d,5.8d);
+		   Identity.printNameAndWeightAndHeight("Maruti",65.8d,5.9d);
+		   Identity.printNameAndWeightAndHeight("Pavan",62.3d,5.5d);
+		   Identity.printNameAndWeightAndHeight("Raj",60.25d,5.8d);
+		   Identity.printNameAndWeightAndHeight("Basu",55.5d,5.9d);
+		   Identity.printNameAndWeightAndHeight("Sunil",62.3d,5.5d);
+		   Identity.printNameAndWeightAndHeight("Ayaz",60.25d,5.8d);
+		   Identity.printNameAndWeightAndHeight("Furqan",55.5d,5.9d);
+		   Identity.printNameAndWeightAndHeight("Nusrat",62.3d,5.5d);
+		   Identity.printNameAndWeightAndHeight("Ravi",60.25d,5.8d);
+ 
+		  
+		 
+		 
+		
 	}
 }
