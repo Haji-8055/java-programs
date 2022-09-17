9f9c364619a8b21321cc1c4e71f6cff7dc740fdc
admin <murtuza.xworkz@gmail.com>
2022-09-17 16:54:16 +0530

    test

---
+++ Groceries.java
@@ -0,0 +1,165 @@
+class Groceries{
+	public static void main(String[] args){
+		
+		int aashirvadAata= 65;
+		int rice= 123;
+		int rava= 29;
+		int besan=58;
+		int idliRava=50;
+		int poha=35;
+		int sabudana=63;
+		int pearlmillet=69;
+		int corn=46;
+		int toorDaal=120;
+		int udadDaal=116;
+		int moongDaal=125;
+		int bengalGram=120;
+		int shenga=130;
+		int putane=115;
+		int kaju=900;
+		int badam=800;
+		int oil= 175;
+		int rusk=80;
+		int biscuit=135;
+		int surfexcel=40;
+		int ponds = 60;
+		int dishSoap =32;
+		int mustard =44;
+		int jeera = 42;
+		int salt = 20;
+		int sugar= 20;
+		int vinegar=170;
+		int peanutButter=250;
+		int teapowder= 68;
+		int jawar = 1200;
+		int wheat=1100; 
+		int horsegram=145;
+		int cookies=52;
+		int chips=22;
+		int pepper=300;
+		int toothpaste=210;
+		int shampoo =550;
+		int noodles=84;
+		int papad=52;
+		int coffePowder=850;
+		int chocolates=113;
+		int soap = 45;
+		int fruitjuice=38;
+		int cheese= 190;
+		int tandooriMasala= 15;
+		int eggs =133;
+		int brush=20;
+		
+		
+		int[] items={aashirvadAata,rice,rava,besan,idliRava,poha,sabudana,pearlmillet,corn,toorDaal
+		            ,udadDaal,moongDaal,bengalGram,shenga,putane,kaju,badam,oil,rusk,biscuit,surfexcel,ponds,dishSoap,mustard,jeera,
+					 salt,sugar,vinegar,peanutButter,teapowder,jawar,wheat,horsegram,cookies,chips,pepper,toothpaste,shampoo,noodles,papad,
+		             coffePowder,chocolates,soap,fruitjuice,cheese,tandooriMasala,eggs,brush};
+		
+		       int total= items.length;
+			   System.out.println("The number of items are"+total);
+			   
+			   int ref0=items[0];
+			   int ref1=items[1];
+			   int ref2=items[2];
+			   int ref3=items[3];
+			   int ref4=items[4];
+			   int ref5=items[5];
+			   int ref6=items[6];
+			   int ref7=items[7];
+			   int ref8=items[8];
+			   int ref9=items[9];
+			   int ref10=items[10];
+			   int ref11=items[11];
+			   int ref12=items[12];
+			   int ref13=items[13];
+			   int ref14=items[14];
+			   int ref15=items[15];
+			   int ref16=items[16];
+			   int ref17=items[17];
+			   int ref18=items[18];
+			   int ref19=items[19];
+			   int ref20=items[20];
+			   int ref21=items[21];
+			   int ref22=items[22];
+			   int ref23=items[23];
+			   int ref24=items[24];
+			   int ref25=items[25];
+			   int ref26=items[26];
+			   int ref27=items[27];
+			   int ref28=items[28];
+			   int ref29=items[29];
+			   int ref30=items[30];
+			   int ref31=items[31];
+			   int ref32=items[32];
+			   int ref33=items[33];
+			   int ref34=items[34];
+			   int ref35=items[35];
+			   int ref36=items[36];
+			   int ref37=items[37];
+			   int ref38=items[38];
+			   int ref39=items[39];
+			   int ref40=items[40];
+			   int ref41=items[41];
+			   int ref42=items[42];
+			   int ref43=items[43];
+			   int ref44=items[44];
+			   int ref45=items[45];
+			   int ref46=items[46];
+			   int ref47=items[47];
+			   
+			   System.out.println("The price of aashirvadAata is " +ref0+" rupees");
+			   System.out.println("The price of rice is " +ref1+" rupees");
+			   System.out.println("The price of rava is " +ref2+" rupees");
+			   System.out.println("The price of besan is " +ref3+" rupees");
+			   System.out.println("The price of idliRava is " +ref4+" rupees");
+			   System.out.println("The price of poha is " +ref5+" rupees");
+               System.out.println("The price of sabudana is " +ref6+" rupees");  
+			   System.out.println("The price of pearlmillet is " +ref7+" rupees");
+			   System.out.println("The price of corn is " +ref8+" rupees");
+			   System.out.println("The price of toorDaal  is " +ref9+" rupees");
+			   System.out.println("The price of udadDaal  is " +ref10+" rupees");
+	       	   System.out.println("The price of moongDaal  is " +ref11+" rupees");
+			   System.out.println("The price of bengalGram  is " +ref12+" rupees");
+			   System.out.println("The price of shenga  is " +ref13+" rupees");
+		       System.out.println("The price of putane is " +ref14+" rupees");
+			   System.out.println("The price of kaju is " +ref15+" rupees");
+			   System.out.println("The price of badam  is " +ref16+" rupees");
+			   System.out.println("The price of oil  is " +ref17+" rupees");
+			   System.out.println("The price of rusk is " +ref18+" rupees");
+			   System.out.println("The price of biscuit is " +ref19+" rupees"); 
+			   System.out.println("The price of surfexcel is " +ref20+" rupees");
+		       System.out.println("The price of ponds is " +ref21+" rupees");
+		       System.out.println("The price of dishSoap is " +ref22+" rupees");
+			   System.out.println("The price of mustard is " +ref23+" rupees");
+			   System.out.println("The price of jeera is " +ref24+" rupees");
+			   System.out.println("The price of salt  is " +ref25+" rupees");
+			   System.out.println("The price of sugar is " +ref26+" rupees");
+			   System.out.println("The price of vinegar is " +ref27+" rupees");
+			   System.out.println("The price of peanutButter is " +ref28+" rupees");
+			   System.out.println("The price of teapowder is " +ref29+" rupees");
+			   System.out.println("The price of jawar is " +ref30+" rupees");
+			   System.out.println("The price of wheat is " +ref31+" rupees");
+			   System.out.println("The price of horsegram  is " +ref32+" rupees");
+			   System.out.println("The price of cookies is " +ref33+" rupees");
+			   System.out.println("The price of chips  is " +ref34+" rupees");
+			   System.out.println("The price of pepper  is " +ref35+" rupees");
+			   System.out.println("The price of toothpaste  is " +ref36+" rupees");
+			   System.out.println("The price of shampoo  is " +ref37+" rupees");
+			   System.out.println("The price of noodles  is " +ref38+" rupees");
+			   System.out.println("The price of papad  is " +ref39+" rupees");
+			   System.out.println("The price of coffePowder  is " +ref40+" rupees");
+			   System.out.println("The price of chocolates is " +ref41+" rupees");
+			   System.out.println("The price of soap  is " +ref42+" rupees");
+			   System.out.println("The price of fruitjuice  is " +ref43+" rupees");
+			   System.out.println("The price of cheese  is " +ref44+" rupees");
+			   System.out.println("The price of tandooriMasala  is " +ref45+" rupees");
+			   System.out.println("The price of eggs  is " +ref46+" rupees");
+			   System.out.println("The price of brush  is " +ref47+" rupees");
+			  
+
+			   
+		   
+			   
+	}
+}
