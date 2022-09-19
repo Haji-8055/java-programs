4fa80e4b46b00c32bc6be49a893c21356f2c1ec0
admin <murtuza.xworkz@gmail.com>
2022-09-19 23:03:06 +0530

    19 september

---
+++ SpiceJet.java
@@ -0,0 +1,12 @@
+class SpiceJet{
+	
+	static void flightNos(String[] flightNos)
+	{
+		System.out.println("Running flightNos");
+		
+		for(int i=0; i<flightNos.length; i++)
+		{
+			System.out.println("flightNo : "+flightNos[i]);
+		}
+	}
+}
---
+++ SpiceJetRunner.java
@@ -0,0 +1,9 @@
+class SpiceJetRunner{
+	
+	public static void main(String[] flights)
+	{
+		System.out.println("Running main method");
+		String[] flightNos={"SG 8194","SG 8912","SG 8561","SG 8473","SG 8481"};
+		SpiceJet.flightNos(flightNos);
+	}
+}
